806dc59395a4fbb80b1d0e57146dd5b609309667
— 𝐋𝐔𝐗𝐘𝐑 ❝💥❞ ☆﹕*ੈ✩‧₊ <luxyrflamedivine@gmail.com>
2026-06-11 18:04:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,49 @@
-document.getElementById("sheetsBtn").onclick = function () {
+document.getElementById("sheetBtn").onclick = function () {
 
 document.getElementById("contenido").innerHTML = `
+
 <h2>Sheet</h2>
 
-<input type="text" placeholder="Buscar plantilla...">
+<input type="text" placeholder="Buscar en la plantilla...">
 
-<p>Aquí aparecerán las plantillas.</p>
+<hr>
+
+<h3>Basic Information</h3>
+
+<p><b>Name:</b></p>
+
+<p><b>Alias:</b></p>
+
+<p><b>Kana:</b></p>
+
+<p><b>Birthday:</b></p>
+
+<hr>
+
+<h3>Pedigree</h3>
+
+<p><b>Sire:</b></p>
+
+<p><b>Dam:</b></p>
+
+<p><b>Damsire:</b></p>
+
+<hr>
+
+<h3>Racing</h3>
+
+<p><b>Surface:</b></p>
+
+<p><b>Distance:</b></p>
+
+<p><b>Running Style:</b></p>
+
+<hr>
+
+<h3>Lore</h3>
+
+<p></p>
+
 `;
 
 };
