7549b0b1787d419c4bb2d414c076c4f948ef120c
— 𝐋𝐔𝐗𝐘𝐑 ❝💥❞ ☆﹕*ੈ✩‧₊ <luxyrflamedivine@gmail.com>
2026-06-11 17:44:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+document.getElementById("sheetBtn").onclick = function () {
 
+document.getElementById("contenido").innerHTML = `
+<h2>Sheet</h2>
+
+<input type="text" placeholder="Buscar plantilla...">
+
+<p>Aquí aparecerán las plantillas.</p>
+`;
+
+};
+
+document.getElementById("umasBtn").onclick = function () {
+
+document.getElementById("contenido").innerHTML = `
+<h2>Umas</h2>
+
+<input type="text" placeholder="Buscar Uma...">
+
+<p>Aquí aparecerán los personajes.</p>
+`;
+
+};
+
+document.getElementById("racesBtn").onclick = function () {
+
+document.getElementById("contenido").innerHTML = `
+<h2>Races</h2>
+
+<input type="text" placeholder="Buscar carrera...">
+
+<p>Aquí aparecerán las carreras.</p>
+`;
+
+};
